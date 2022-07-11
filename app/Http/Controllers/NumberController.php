d7e555dffe90ed1ee748b712091dc6ddc31cd250
mborowik <marcin@borowik.pro>
2022-07-11 20:42:50 +0200

    cd

--- app/Http/Controllers/NumberController.php
+++ app/Http/Controllers/NumberController.php
@@ -4,12 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Number;
 use App\Models\Provider;
+use App\Models\Extension;
+use AhmadMayahi\Polly\Polly;
+use AhmadMayahi\Polly\Config;
 use Illuminate\Support\Facades\DB;
+use AhmadMayahi\Polly\Voices\Polish;
+use Google\Cloud\Storage\StorageClient;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Redirect;
 use App\Http\Requests\StoreNumberRequest;
 use App\Http\Requests\StoreNumbersRequest;
 use App\Http\Requests\UpdateNumberRequest;
-use App\Models\Extension;
+use Google\Cloud\TextToSpeech\V1\AudioConfig;
+use Google\Cloud\TextToSpeech\V1\AudioEncoding;
+use Google\Cloud\TextToSpeech\V1\SynthesisInput;
+use Google\Cloud\TextToSpeech\V1\SsmlVoiceGender;
+use AhmadMayahi\Polly\Voices\English\UnitedStates;
+use Google\Cloud\TextToSpeech\V1\TextToSpeechClient;
+use Google\Cloud\TextToSpeech\V1\VoiceSelectionParams;
 
 class NumberController extends Controller
 {
@@ -20,6 +32,67 @@ class NumberController extends Controller
      */
     public function index()
     {
+        $config = (new Config())
+            ->setKey('AKIAICUIKDENRY44ZFQQ')
+            ->setSecret('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')
+            ->setRegion('us-east-1'); // default is: us-east-1
+
+
+        $polly = Polly::init($config);
+
+        $speechFile = $polly
+                        ->voiceId(Polish::Maja)
+                        
+                        // Ewa Maja Jacek Jan
+                        // Default is MP3.
+                        // Available options: asMp3(), asOgg(), asPcm(), asJson()
+                        ->asMp3()
+                        ->text(' Witamy .. tu centrum Venus, cieszymy się że do nas dzwonisz, nagrywamy tę rozmowę bo chcemy dać ci najlepszą jakość naszych usług.')
+                        ->convert('plik.mp3');
+
+        die();
+        // https://cloud.google.com/text-to-speech/docs/voices
+        
+        // Polish (Poland)	Standard	pl-PL	pl-PL-Standard-A	FEMALE
+        // Polish (Poland)	Standard	pl-PL	pl-PL-Standard-B	MALE
+        // Polish (Poland)	Standard	pl-PL	pl-PL-Standard-C	MALE
+        // Polish (Poland)	Standard	pl-PL	pl-PL-Standard-D	FEMALE
+        // Polish (Poland)	Standard	pl-PL	pl-PL-Standard-E	FEMALE
+        // Polish (Poland)	WaveNet	pl-PL	pl-PL-Wavenet-A	FEMALE
+        // Polish (Poland)	WaveNet	pl-PL	pl-PL-Wavenet-B	MALE
+        // Polish (Poland)	WaveNet	pl-PL	pl-PL-Wavenet-C	MALE
+        // Polish (Poland)	WaveNet	pl-PL	pl-PL-Wavenet-D	FEMALE
+        // Polish (Poland)	WaveNet	pl-PL	pl-PL-Wavenet-E	FEMALE
+
+        putenv("GOOGLE_APPLICATION_CREDENTIALS=impactful-water-351318-084f0cbadd2e.json");
+
+
+
+ 
+
+        $textToSpeechClient = new TextToSpeechClient();
+        $name = 'pl-PL-Wavenet-A';
+        $input = new SynthesisInput();
+        $input->setSsml('<speak>Witam, <break time="700ms"/>tu centrum Venus,<break time="700ms"/> cieszymy się że do nas dzwonisz, nagrywamy tę rozmowę bo chcemy dać ci najlepszą jakość naszych usług. Jeśli nie chcesz być nagrywanym to oczywiście w każdym momencie możesz się rozłączyć, prosimy poczekaj chwile na linii aż połączy się z tobą pracownik</speak>');
+        $voice = new VoiceSelectionParams();
+        $voice->setLanguageCode('pl-PL');
+        $voice->setName($name);
+       
+
+        //  $voice->setSsmlGender(SsmlVoiceGender::MALE);
+        $audioConfig = new AudioConfig();
+        $audioConfig->setAudioEncoding(AudioEncoding::MP3);
+        //  $audioConfig->setSampleRateHertz(8000);
+        $audioConfig->setVolumeGainDb(16.0);
+
+        $resp = $textToSpeechClient->synthesizeSpeech($input, $voice, $audioConfig);
+        file_put_contents($name.'.mp3', $resp->getAudioContent());
+        die();
+     
+     
+     
+     
+     
         return view('number.index', ['numbers' => Number::all()]);
     }
 
@@ -69,7 +142,7 @@ class NumberController extends Controller
             $this->dodajkierowanie($number, $provider->uniqid);
             $this->dodajkierowaniealarmowe($number, $nka);
         } catch (\Exception  $e) {
-           // print_r($e);
+            // print_r($e);
         }
 
 
@@ -100,7 +173,6 @@ class NumberController extends Controller
             $numbers[] = ['provider_uniqid' => $provider->uniqid, 'number' => $i];
             $this->dodajkierowanie($i, $provider->uniqid);
             $this->dodajkierowaniealarmowe($i, $nka);
-            
         }
       
         
@@ -200,7 +272,7 @@ class NumberController extends Controller
         ]);
     }
 
-    public function dodajkierowaniealarmowe($number,$nka)
+    public function dodajkierowaniealarmowe($number, $nka)
     {
         foreach ($nka as $n) {
             try {
